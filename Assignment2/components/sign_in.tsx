6d22d50c8f51504755c65b23e1f2d3bb13984ddf
Cody Nguyen <Thanh.ng13@hotmail.com>
2025-02-11 14:03:28 -0700

    created the validation for the credentials

--- Assignment2/components/sign_in.tsx
+++ Assignment2/components/sign_in.tsx
@@ -1,20 +1,45 @@
-import { View, Text, TextInput, StyleSheet, Button} from "react-native";
+import { View, Text, TextInput, StyleSheet, Button, Alert} from "react-native";
 import { useState } from "react";
 import credentials from "../credentials.json";
 
 type Sign_inProps = { setIsSignedIn: ( isSignedIn: boolean) => void };
 
 const Sign_in: React.FC<Sign_inProps> = ({setIsSignedIn}) => {
-    const handleSignIn = () => {setIsSignedIn(true)}
     const [Username,setUserName] = useState<string>("");
     const [Password,setPassword] = useState<string>("");
+    const handleSignIn = () => {
+        const user= credentials.users.find((user) => user.username === Username && user.password === Password );
+
+        if(!user) {
+            alert("Invalid Username or Password")
+        } else {
+            setIsSignedIn(true) 
+        }
+    };
+    const validateCredentials = () =>{
+        const user= credentials.users.find((user) => user.username === Username && user.password === Password );
+        const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+        if (Username.length < 5) {
+            Alert.alert('Error', 'Username must be at least 5 characters long.');
+            return;
+          }
+        if (!passwordRegex.test(Password)) {
+            Alert.alert('Error', 'Password must be at least 8 characters, include uppercase, lowercase, number, and special character.');
+            return;
+          }
+        if(!user) {
+            alert("Invalid Username or Password")
+        } else {
+            setIsSignedIn(true) 
+        }
+    };
 
     return (
         <View style={styles.container}>
             <Text style={styles.title}>Sign In</Text>
             <TextInput placeholder="Username" style={styles.input} onChangeText={setUserName} value={Username}/>
-            <TextInput placeholder="Password" style={styles.input} onChangeText={setPassword} value={Password}/>
-            <Button title = "Sign In" onPress = {handleSignIn}/>
+            <TextInput placeholder="Password" style={styles.input} onChangeText={setPassword} value={Password} secureTextEntry={true}/>
+            <Button title = "Sign In" onPress = {validateCredentials}/>
         </View>
     );
 };
