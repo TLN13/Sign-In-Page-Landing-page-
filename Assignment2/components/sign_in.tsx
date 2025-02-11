de1f6f80c80ffbc04b5667a1d299ee6245b5fad9
Cody Nguyen <Thanh.ng13@hotmail.com>
2025-02-11 13:22:38 -0700

    Finish implementing logic for sign in page and added the credientials.json file

--- Assignment2/components/sign_in.tsx
+++ Assignment2/components/sign_in.tsx
@@ -1,11 +1,17 @@
-import { View, Text, TextInput, StyleSheet} from "react-native";
-
+import { View, Text, TextInput, StyleSheet, Button} from "react-native";
+import { useState } from "react";
+import credentials from "../credentials.json";
 const Sign_in = () => {
+    const handleSignIn = () => {alert ("Sign In pressed")}
+    const [Username,setUserName] = useState("");
+    const [Password,setPassword] = useState("");
+
     return (
         <View style={styles.container}>
             <Text style={styles.title}>Sign In</Text>
-            <TextInput placeholder="Username" style={styles.input}></TextInput>
-            <TextInput placeholder="Password" style={styles.input}></TextInput>
+            <TextInput placeholder="Username" style={styles.input} onChangeText={setUserName} value={Username}/>
+            <TextInput placeholder="Password" style={styles.input} onChangeText={setPassword} value={Password}/>
+            <Button title = "Sign In" onPress = {handleSignIn}/>
         </View>
     );
 };
@@ -27,7 +33,10 @@ const styles = StyleSheet.create({
         borderWidth: 1,
         marginVertical: 10,
         borderRadius: 5,
-      },
+    },
+    button: {
+
+    },
 })
 
 export default Sign_in;
